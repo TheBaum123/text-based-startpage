29420bcfa5e0c9119b59c9cea7d0a5f2f348dd86
TheBaum123 <102868970+TheBaum123@users.noreply.github.com>
2023-04-18 23:39:36 +0200

    reload instantly after getting defaults

--- scripts/getPreferences.js
+++ scripts/getPreferences.js
@@ -1,11 +1,19 @@
+//search engine
 let selectedSearchEngine = ""
 localStorage.getItem("text-startpage:selectedSearchEngine") ? selectedSearchEngine = localStorage.getItem("text-startpage:selectedSearchEngine") : selectedSearchEngine = "duckduckgo"
+
+//greeting
 let enableGreeting = "" 
 localStorage.getItem("text-startpage:enableGreeting") ? enableGreeting = localStorage.getItem("text-startpage:enableGreeting") : enableGreeting = false
 let greetingText = ""
 localStorage.getItem("text-startpage:greetingText") ? greetingText = localStorage.getItem("text-startpage:greetingText") : greetingText = "Hello"
 let greetingName = localStorage.getItem("text-startpage:greetingName")
-let bookmarks = localStorage.getItem("text-startpage:bookmarks")
+
+//clock
+let clockSeperator = localStorage.getItem("text-startpage:clockSeperator")
+
+//bookmarks
+let bookmarks = JSON.parse(localStorage.getItem("text-startpage:bookmarks"))
 if(!bookmarks) {
     const xhr = new XMLHttpRequest()
     xhr.open("GET", "json/defaultBookmarks.json")
@@ -14,9 +22,7 @@ if(!bookmarks) {
     xhr.onload = () => {
         if(xhr.status == 200) {
             localStorage.setItem("text-startpage:bookmarks", JSON.stringify(xhr.response))
-            setTimeout(() => {
-                window.location.reload
-            }, 1000);
+            window.location.reload()
         } else {
             const errorSpan = document.createElement("span")
             const errorText = document.createTextNode("ERROR: " + xhr.status)
