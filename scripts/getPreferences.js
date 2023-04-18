626c4fb8ff64224b4f9f6d8ecccaa9f22571572f
TheBaum123 <102868970+TheBaum123@users.noreply.github.com>
2023-04-18 21:35:03 +0200

    added getting/setting bookmarks to preferences

--- scripts/getPreferences.js
+++ scripts/getPreferences.js
@@ -5,3 +5,23 @@ localStorage.getItem("text-startpage:enableGreeting") ? enableGreeting = localSt
 let greetingText = ""
 localStorage.getItem("text-startpage:greetingText") ? greetingText = localStorage.getItem("text-startpage:greetingText") : greetingText = "Hello"
 let greetingName = localStorage.getItem("text-startpage:greetingName")
+let bookmarks = localStorage.getItem("text-startpage:bookmarks")
+if(!bookmarks) {
+    const xhr = new XMLHttpRequest()
+    xhr.open("GET", "json/defaultBookmarks.json")
+    xhr.responseType = "json"
+    xhr.send()
+    xhr.onload = () => {
+        if(xhr.status == 200) {
+            localStorage.setItem("text-startpage:bookmarks", JSON.stringify(xhr.response))
+            setTimeout(() => {
+                window.location.reload
+            }, 1000);
+        } else {
+            const errorSpan = document.createElement("span")
+            const errorText = document.createTextNode("ERROR: " + xhr.status)
+            errorSpan.appendChild(errorText)
+            document.getElementById("bookmarks-container").appendChild(errorSpan)
+        }
+    }
+}
