67526dc02145fd4fd6f2cf2ed139f1901689abf8
TheBaum123 <102868970+TheBaum123@users.noreply.github.com>
2023-04-19 20:29:34 +0200

    add settings button

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,7 @@
 let injector = document.createElement("script")
 injector.src = "scripts/injector.js"
 document.body.appendChild(injector)
+
+document.querySelector("ion-icon[name=\"settings-outline\"]").addEventListener("click", () => {
+    window.location.assign("/settings.html")
+})
