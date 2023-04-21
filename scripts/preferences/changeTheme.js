bf7e71c506c9c47952fe12dbbd46b57a799b4291
TheBaum123 <102868970+TheBaum123@users.noreply.github.com>
2023-04-21 22:52:45 +0200

    can now define custom colors

--- scripts/preferences/changeTheme.js
+++ scripts/preferences/changeTheme.js
@@ -1,5 +1,28 @@
-document.getElementById("theme-selector").value = localStorage.getItem("text-startpage:theme")
+const dropdown = document.getElementById("theme-selector")
+const customSelectorsWrapper = document.getElementById("custom-color-selector")
+const selectionWrapperChildren = customSelectorsWrapper.children
 
-document.getElementById("theme-selector").addEventListener("change", () => {
-    localStorage.setItem("text-startpage:theme", document.getElementById("theme-selector").value)
+dropdown.value = localStorage.getItem("text-startpage:theme")
+
+if(dropdown.value == "custom") {
+    customSelectorsWrapper.style.display = "flex"
+} else {
+    customSelectorsWrapper.style.display = "none"
+}
+
+dropdown.addEventListener("change", () => {
+    if(dropdown.value == "custom") {
+        customSelectorsWrapper.style.display = "flex"
+    } else {
+        customSelectorsWrapper.style.display = "none"
+    }
+    localStorage.setItem("text-startpage:theme", dropdown.value)
+})
+
+Object.values(selectionWrapperChildren).forEach(e => {
+    if(e.tagName == "INPUT") {
+        e.addEventListener("change", () => {
+            localStorage.setItem("text-startpage:custom-" + e.id, e.value)
+        })
+    }
 })
