e64cba0ee437ecc5c2f030c16f158e9d47395691
TheBaum123 <102868970+TheBaum123@users.noreply.github.com>
2024-01-20 02:05:10 +0100

    automatically add opacity when a custop background is applied in order to improve visibility of the background (reduces contrast depending on the background image chosen)

--- scripts/wallpaper.js
+++ scripts/wallpaper.js
@@ -8,6 +8,14 @@ setTimeout(() => {
         }
 
         document.body.style.backgroundImage = `url("${wallpapersLinksArray[wallpapersCycleIndex]}")`;
+        document.getElementById("bookmarks-container").style.backgroundColor =
+            "color-mix(in oklab, #00000000 15%, var(--light-background-color) 85%)";
+        document.getElementById("greetings-container").style.backgroundColor =
+            "color-mix(in oklab, #00000000 15%, var(--light-background-color) 85%)";
+        document.getElementById(
+            "settings-icon-top-right"
+        ).style.backgroundColor =
+            "color-mix(in oklab, #00000000 15%, var(--light-background-color) 85%)";
 
         wallpapersCycleIndex++;
         localStorage.setItem(
