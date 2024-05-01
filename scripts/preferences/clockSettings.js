28fb3ab1da264b6ad47bf2ea57533d6eb059042b
TheBaum123 <102868970+TheBaum123@users.noreply.github.com>
2024-05-01 17:32:53 +0200

    add preference for date seperator in clock on top of the page

--- scripts/preferences/clockSettings.js
+++ scripts/preferences/clockSettings.js
@@ -1,27 +1,47 @@
-const enableClockInput = document.getElementById("enable-clock")
-const clockSeperatorInput = document.getElementById("clock-seperator-input")
+const enableClockInput = document.getElementById("enable-clock");
+const clockSeperatorInput = document.getElementById("clock-seperator-input");
+const dateSeperatorInput = document.getElementById("date-seperator-input");
 
-let enableClock = localStorage.getItem("text-startpage:enableClock")
-let clockSeperator = localStorage.getItem("text-startpage:clockSeperator")
+let enableClock = localStorage.getItem("text-startpage:enableClock");
+let clockSeperator = localStorage.getItem("text-startpage:clockSeperator");
+let dateSeperator = localStorage.getItem("text-startpage:dateSeperator");
 
-if(enableClock == "true") enableClockInput.checked = true
-if(clockSeperator) clockSeperatorInput.value = clockSeperator
+if (enableClock == "true") enableClockInput.checked = true;
+if (clockSeperator) clockSeperatorInput.value = clockSeperator;
+if (dateSeperator) dateSeperatorInput.value = dateSeperator;
 
 enableClockInput.addEventListener("change", () => {
-    if(!enableClockInput.checked) {
-        localStorage.setItem("text-startpage:enableClock", false)
-        localStorage.setItem("text-startpage:clockSeperator", "")
-        clockSeperatorInput.value = ""
+    if (!enableClockInput.checked) {
+        localStorage.setItem("text-startpage:enableClock", false);
+        localStorage.setItem("text-startpage:clockSeperator", "");
+        clockSeperatorInput.value = "";
     } else {
-        localStorage.setItem("text-startpage:enableClock", true)
-        localStorage.setItem("text-startpage:clockSeperator", clockSeperatorInput.value)
+        localStorage.setItem("text-startpage:enableClock", true);
+        localStorage.setItem(
+            "text-startpage:clockSeperator",
+            clockSeperatorInput.value
+        );
     }
-})
+});
 
 clockSeperatorInput.addEventListener("change", () => {
-    if(!enableClockInput.checked) {
-        localStorage.setItem("text-startpage:clockSeperator", "")
+    if (!enableClockInput.checked) {
+        localStorage.setItem("text-startpage:clockSeperator", "");
     } else {
-        localStorage.setItem("text-startpage:clockSeperator", clockSeperatorInput.value)
+        localStorage.setItem(
+            "text-startpage:clockSeperator",
+            clockSeperatorInput.value
+        );
     }
-})
+});
+
+dateSeperatorInput.addEventListener("change", () => {
+    if (!enableClockInput.checked) {
+        localStorage.setItem("text-startpage:dateSeperator", "");
+    } else {
+        localStorage.setItem(
+            "text-startpage:dateSeperator",
+            dateSeperatorInput.value
+        );
+    }
+});
